96f2305bf4045d196e85024539600b5b4a10f70e
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-09-04 22:06:12 +0200

    beta-1.0.0

--- DocumentationCommentAndNaming.ConsoleApp.Example/Program.cs
+++ DocumentationCommentAndNaming.ConsoleApp.Example/Program.cs
@@ -16,6 +16,7 @@ namespace DocumentationCommentAndNaming.ConsoleApp.Example
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
