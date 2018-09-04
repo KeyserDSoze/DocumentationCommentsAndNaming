3a3639ef2de9fe9f8101de95296a926b46cd0873
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-09-04 23:03:03 +0200

    beta-1.0.0

--- DocumentationCommentAndNaming.Library.Database/CustomerBase.cs
+++ DocumentationCommentAndNaming.Library.Database/CustomerBase.cs
@@ -34,10 +34,28 @@ namespace DocumentationCommentAndNaming.Library.Database
     public class CustomerBase
     {
         public int UserId { get; set; }
+        /// <summary>
+        /// Retrieve user from DB.
+        /// </summary>
+        /// <returns>
+        /// Nothing but populate this class.
+        /// </returns>
+        /// <exception cref="System.Exception">Thrown when one parameter is null or zero.</exception>
+        /// See <see cref="Get()"/> to perform a retrieve.
+        /// <seealso cref="Update()"/>
         public void Get()
         {
             this.UserId = 1;
         }
+        /// <summary>
+        /// Save user on DB.
+        /// </summary>
+        /// <returns>
+        /// Nothing.
+        /// </returns>
+        /// <exception cref="System.Exception">Thrown when one parameter is null or zero.</exception>
+        /// See <see cref="Update()"/> to perform an update on DB.
+        /// <seealso cref="Get()"/>
         public void Update()
         {
             Console.WriteLine("Save in DB");
